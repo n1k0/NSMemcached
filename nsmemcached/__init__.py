a8bf5843e17de0da41ee09000961eefb55d9c541
Nicolas Perriault <nperriault@gmail.com>
2011-05-06 09:58:22 +0200

    added a comvinient Client class in module __init__ to ease import and instantation

--- nsmemcached/__init__.py
+++ nsmemcached/__init__.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
 
-VERSION = (0, 1, 0)
+import memcache
+
+from client import NamespacedClient
+
+VERSION = (0, 2, 0)
 __version__ = '.'.join([str(x) for x in VERSION])
+
+
+class Client(NamespacedClient):
+    """ Just a convenient Client class to ease import and instantiation::
+
+        >>> from nsmemcached import Client
+        >>> ns_client = Client(['127.0.0.1:11211'])
+    """
+    def __init__(self, *args, **kwargs):
+        super(Client, self).__init__(memcache.Client(*args, **kwargs))
